02e909dc722387f6c1cf6940a790b1c84cc6abb7
AmandaFI <a.ferrari.iaquinta@gmail.com>
2023-06-02 14:36:25 -0300

    lolcalhost != 127.0.0.1

--- src/services/api.tsx
+++ src/services/api.tsx
@@ -1,17 +1,5 @@
 import axios from "axios";
 
-// import { wrapper } from 'axios-cookiejar-support';
-// import { CookieJar } from 'tough-cookie';
-
-// declare module 'axios' {
-// 	interface AxiosRequestConfig {
-// 		jar?: CookieJar;
-//   }
-// }
-
-// const jar = new CookieJar();
-// const client = wrapper(axios.create({ jar }));
-
 export type authenticationType = {
 	email: string;
 	password: string;
@@ -61,33 +49,13 @@ export type completeListType = {
 	items: Array<listItemType>;
 };
 
-export const axiosInstance = axios.create({
-	withCredentials: true,
-	baseURL: "http://127.0.0.1:3000/api/"
-});
 
-// axios.defaults.withCredentials = true;
-// axios.defaults.baseURL = "http://192.168.15.7:3000/api";
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = 'http://localhost:3000/api/'
 
-// axiosInstance.interceptors.request.use((x) => {
-// 	console.log(x);
-// 	return x;
-// });
-
-// axiosInstance.interceptors.response.use((x) => {
-// 	console.log(x);
-// 	return x;
-// });
+export const login = (info: authenticationType) => axios.post<loggedUserType>('/sessions', info)
+export const loginStatus = () => axios.get<loggedUserType>('/sessions/status')
+export const createUser = (newUser: userType) => axios.post<loggedUserType>('/users', newUser)
+export const loadFeed = () => axios.get<Array<completeListType>>('/users/followed_users_lists')
 
 
-export const login = (info: authenticationType) =>
-	axiosInstance.post<loggedUserType>("/sessions", info, {
-	});
-
-export const createUser = (newUser: userType) =>
-	axiosInstance.post<loggedUserType>("/users", newUser, {
-	});
-
-export const loadFeed = () =>
-	axiosInstance.get<Array<completeListType>>("/users/followed_users_lists", {
-	});
